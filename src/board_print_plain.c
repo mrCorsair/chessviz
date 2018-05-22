61c8feea7510ad11e694b1732c3902c0919f0d77
Corsair <mrCorsair@github.com>
2018-05-22 12:08:03 +0700

    Travic:for

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -5,14 +5,16 @@ extern int x1,y11,x2,y2;
 extern char desk[9][9];
 
 void printdesk (){
-	for(int i=0;i<8;i++){
+	int i=0,j=0;
+	for(i;i<8;i++){
 	printf("%d|",i+1);
-	for(int j=0;j<8;j++){
+	for(j;j<8;j++){
 			printf("%c",desk[i][j]);
 		}
 			printf("\n");
 	}
 	printf("  ");
-	for(int i=0;i<8;i++)printf("%c",i+65);
+	i=0;
+	for(i;i<8;i++)printf("%c",i+65);
 	printf("\n");
 }
