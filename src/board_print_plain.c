13353cf2748c6859350bdfb5c111da80bee41723
Corsair <mrCorsair@github.com>
2018-05-22 12:14:37 +0700

    Travis^for2

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -5,16 +5,15 @@ extern int x1,y11,x2,y2;
 extern char desk[9][9];
 
 void printdesk (){
-	int i=0,j=0;
-	for(i;i<8;i++){
+	int i,j;
+	for(i=0;i<8;i++){
 	printf("%d|",i+1);
-	for(j;j<8;j++){
+	for(j=0;j<8;j++){
 			printf("%c",desk[i][j]);
 		}
 			printf("\n");
 	}
 	printf("  ");
-	i=0;
-	for(i;i<8;i++)printf("%c",i+65);
+	for(i=0;i<8;i++)printf("%c",i+65);
 	printf("\n");
 }
